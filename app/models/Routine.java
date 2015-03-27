c13849123a7542847787f7cfcdb02677dd34f611
David Neely <davidkneely>
2015-03-27 11:32:00 -1000

    Updated Routine model to reflect Routine attributes. #47

--- app/models/Routine.java
+++ app/models/Routine.java
@@ -1,28 +1,43 @@
 package models;
 
 /**
- * A contact object that holds a First Name, Last Name, and Telephone Number.
+ * A routine object that holds a name, image, magic type, skill level,
+ * information, description, and materials associated with the routine.
  */
 public class Routine {
+
   private long id;
-  private String firstName;
-  private String lastName;
-  private String telephone;
+  private String name;
+  private String image;
+  private String magicType;
+  private String skillLevel;
+  private String info;
+  private String description;
+  private String materials;
 
   /**
-   * Create new Contact object.
+   * Create new Routine object.
    *
-   * @param id the id value.
-   * @param firstName the user first name.
-   * @param lastName  the user last name.
-   * @param telephone the user telephone number.
+   * @param id The id.
+   * @param name The name of the routine.
+   * @param image The image associated with the routine.
+   * @param magicType The type of magic associated with the routine.
+   * @param skillLevel The skill level required to do this routine.
+   * @param info The information about this routine.
+   * @param description The description of this routine.
+   * @param materials The materials needed to perform this routine.
    */
-  public Routine(long id, String firstName, String lastName, String telephone) {
+  public Routine(long id, String name, String image, String magicType, String skillLevel,
+                 String info, String description, String materials) {
 
     this.id = id;
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.telephone = telephone;
+    this.name = name;
+    this.image = image;
+    this.magicType = magicType;
+    this.skillLevel = skillLevel;
+    this.info = info;
+    this.description = description;
+    this.materials = materials;
   }
 
   /**
@@ -35,29 +50,66 @@ public class Routine {
   }
 
   /**
-   * Returns the first name value to the caller.
+   * Returns the name value to the caller.
    *
-   * @return firstName String.
+   * @return The name string.
    */
-  public String getFirstName() {
-    return firstName;
+  public String getName() {
+    return name;
   }
 
   /**
-   * Returns the last name value to the caller.
+   * Returns the image value to the caller.
    *
-   * @return lastName String.
+   * @return The image string.
    */
-  public String getLastName() {
-    return lastName;
+  public String getImage() {
+    return image;
   }
 
   /**
-   * Returns the telephone number value to the caller.
+   * Returns the magic type value to the caller.
    *
-   * @return telephone String.
+   * @return The magic type string.
    */
-  public String getTelephone() {
-    return telephone;
+  public String getMagicType() {
+    return magicType;
   }
+
+  /**
+   * Returns the skill level value to the caller.
+   *
+   * @return The skill level string.
+   */
+  public String getSkillLevel() {
+    return skillLevel;
+  }
+
+  /**
+   * Returns the info value to the caller.
+   *
+   * @return The info string.
+   */
+  public String getInfo() {
+    return info;
+  }
+
+  /**
+   * Returns the description value to the caller.
+   *
+   * @return The description string.
+   */
+  public String getDescription() {
+    return description;
+  }
+
+  /**
+   * Returns the materials value to the caller.
+   *
+   * @return The materials string.
+   */
+  public String getMaterials() {
+    return materials;
+  }
+
 }
