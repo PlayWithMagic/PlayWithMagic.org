47e86375ca9651edb043b7e7036fcb87fd798819
davidkneely <davidics613@gmail.com>
2015-05-03 18:11:50 -1000

    Updated java docs in Image.java to pass CheckStyle. #40

--- app/models/Image.java
+++ app/models/Image.java
@@ -13,38 +13,58 @@ import java.io.BufferedInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
 
+/**
+ * Image holds the image associated with a magician, routine, or material.
+ * Images are not required.
+ */
 @Entity
 public class Image extends Model {
   private static final long serialVersionUID = 1L;
 
+  /** The image id. */
   @Id
   public long id;
 
+  /** The image name. */
   @Constraints.Required
   public String name;
 
+  /** The image data. */
   @Lob
   public byte[] data;
 
+  /**
+   * The EBean ORM finder method for database queries.
+   *
+   * @return The finder method.
+   */
   public static Finder<Long, Image> find =
       new Finder<Long, Image>(Long.class, Image.class);
 
+  /**
+   * Creates a new image entity.
+   * @param name The name of the image.
+   * @param image The byte array with the image data.
+   */
   public Image(String name, File image) {
     this.name = name;
-    this.data = new byte[(int)image.length()];
+    this.data = new byte[(int) image.length()];
 
     /* write the image data into the byte array */
     InputStream inStream = null;
     try {
       inStream = new BufferedInputStream(new FileInputStream(image));
       inStream.read(this.data);
-    } catch (IOException e) {
+    }
+    catch (IOException e) {
       e.printStackTrace();
-    } finally {
+    }
+    finally {
       if (inStream != null) {
         try {
           inStream.close();
-        } catch (IOException e) {
+        }
+        catch (IOException e) {
           e.printStackTrace();
         }
       }
